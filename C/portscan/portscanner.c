b3ec1031e326413bf471b27607e5a973d00b5ec7
pperfeto <paulo.perfetto@gmail.com>
2022-11-22 22:31:31 -0300

    refactored portscan

--- C/portscan/portscanner.c
+++ C/portscan/portscanner.c
@@ -7,31 +7,37 @@ int main(int argc, char *argv[]) {
   int meusocket;
   int conecta;
   
-  int porta;
   int final = 65535;
   char *destino = argv[1];
 
   struct sockaddr_in alvo;
-
-  for(porta=0;porta<final;porta++) {
-
-    meusocket = socket(AF_INET, SOCK_STREAM, 0);
   
-    alvo.sin_family = AF_INET;
-    alvo.sin_port = htons(porta);
-    alvo.sin_addr.s_addr = inet_addr(destino);
+  if(argc < 2) {
+    printf("Modo de uso: ./portscan 192.168.0.1\n");
+  }
+  else {
 
-    conecta = connect(meusocket, (struct sockaddr *)&alvo, sizeof alvo);
+    for(int porta = 0; porta < final; porta++) {
 
-    if(conecta == 0) {
-      printf("Porta %d - status [ABERTA]\n", porta);
-      close(meusocket);
-      close(conecta);
-    }
-    else {
-      close(meusocket);
-      close(conecta);
+      meusocket = socket(AF_INET, SOCK_STREAM, 0);
+    
+      alvo.sin_family = AF_INET;
+      alvo.sin_port = htons(porta);
+      alvo.sin_addr.s_addr = inet_addr(destino);
+
+      conecta = connect(meusocket, (struct sockaddr *)&alvo, sizeof alvo);
+
+      if(conecta == 0) {
+        printf("Porta %d - status [ABERTA]\n", porta);
+        close(meusocket);
+        close(conecta);
+      }
+      else {
+        close(meusocket);
+        close(conecta);
+      }
     }
   }
 
+  return 0;
 }
